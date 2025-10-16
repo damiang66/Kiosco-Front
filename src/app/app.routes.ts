8c5fa866948a2b58c6eae5c1087e66511ce8d30b
UlisesGS <ulisesgadea28@gmail.com>
2025-10-16 20:51:16 -0300

    cambiando routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { ProductoListComponent } from './features/productos/components/producto-
 import { ProductoFormComponent } from './features/productos/components/producto-form/producto-form.component';
 import { CompraListComponent } from './features/compra/compra-list/compra-list.component';
 import { CompraFormComponent } from './features/compra/compra-form/compra-form.component';
-import { VentaListComponent } from './features/ventas/venta-list/venta-list.component';
-import { VentaFormComponent } from './features/ventas/venta-form/venta-form.component';
+import { VentaListComponent } from './features/ventas/components/venta-list/venta-list.component';
+import { VentaFormComponent } from './features/ventas/components/venta-form/venta-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'productos', pathMatch: 'full' },
@@ -13,11 +13,10 @@ export const routes: Routes = [
   { path: 'productos/editar/:id', component: ProductoFormComponent },
   { path: 'compras', component: CompraListComponent },
   { path: 'compras/nueva', component: CompraFormComponent },
- { path: 'compras/editar/:id', component: CompraFormComponent },
- { path: 'ventas', component: VentaListComponent },
+  { path: 'compras/editar/:id', component: CompraFormComponent },
+  { path: 'ventas', component: VentaListComponent },
   { path: 'ventas/nuevo', component: VentaFormComponent },
- { path: 'ventas/editar/:id', component: VentaFormComponent },
-
+  { path: 'ventas/editar/:id', component: VentaFormComponent },
   { path: '**', redirectTo: 'productos' },
 ];
 
